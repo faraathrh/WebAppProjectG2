a16420ea739282fe0a56a605957b67a0f0b8914a
deenz <136409314+irdinarahman@users.noreply.github.com>
2025-06-12 10:06:23 +0800

    POS SYSTEM WEB.PHP
    ONLY FOR POS SYSTEM PLSSS

--- web.php
+++ web.php
@@ -1,22 +1,41 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoyaltyController;
+use App\Http\Controllers\POSController;
 
-// Main Page Route
 Route::get('/', function () {
-    return view('mainpage');
-})->name('home');
-
-// Route::get('/', function () {
-//     return redirect()->route('index');
-// });
-
-Route::prefix('loyalty')->group(function () {
-    Route::get('/', [LoyaltyController::class, 'index'])->name('index');
-    Route::get('/create', [LoyaltyController::class, 'create'])->name('create');
-    Route::post('/', [LoyaltyController::class, 'store'])->name('store');
-    Route::get('/{id}', [LoyaltyController::class, 'show'])->name('show');
-    Route::post('/{customerId}/transaction', [LoyaltyController::class, 'addTransaction'])
-        ->name('loyalty.addTransaction');
+    return view ('mainpage');;
 });
+
+// POS main page
+Route::get('/POS', [POSController::class, 'index'])->name('POS');
+
+// Add item to cart
+Route::post('/POS/add', [POSController::class, 'addToCart'])->name('add.to.cart');
+
+// View checkout page
+Route::get('/checkout', [POSController::class, 'checkout'])->name('checkout');
+
+// Process checkout (store order, etc.)
+Route::post('/checkout/process', [POSController::class, 'processCheckout'])->name('checkout.process');
+
+// Show receipt
+Route::get('/receipt', function () {
+    return view('receipt');
+})->name('receipt');
+
+// Remove specific item from cart
+Route::get('/removeFromCart/{index}', function ($index) {
+    $cart = session('cart', []);
+    unset($cart[$index]); // remove by array index
+    session(['cart' => array_values($cart)]); // reindex
+    return redirect('/POS'); // or your POS route
+})->name('cart.remove');
+
+
+Route::get('/cart/clear', [POSController::class, 'clearCart'])->name('cart.clear');
+
+Route::get('/empty-cart', function () {
+    session()->forget('cart');
+    return redirect()->route('POS')->with('success', 'Cart emptied.');
+})->name('cart.empty');
